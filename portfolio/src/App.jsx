51f86f2afeb58097b00384e7f0a7476e3ce16c4a
Rohan <shingaderohan96@gmail.com>
2024-02-17 18:46:57 +0530

    contact form done & working

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Main from './components/Main'
 import Sidenav from './components/Sidenav'
 import Work from './components/Work'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Main/>
       <Work/>
       <Projects/>
+      <Contact/>
     </>
   )
 }
