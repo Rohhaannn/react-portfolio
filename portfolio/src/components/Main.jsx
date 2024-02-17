fb205be8b0e523ef0a329e5fd791636ecf9bf6fe
Rohan <shingaderohan96@gmail.com>
2024-02-17 19:31:18 +0530

    social media links updated

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import { FaTwitter, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { FaTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -12,7 +13,10 @@ const Main = () => {
 
       <div className="w-full h-screen absolute top-0 left-0 bg-white/20">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-gray-200"> I'm Rohan Shingade</h1>
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-200">
+            {" "}
+            I'm Rohan Shingade
+          </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-200 font-semibold">
             {" "}
             I'm a
@@ -22,23 +26,34 @@ const Main = () => {
                 "MERN Stack Developer",
                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                 "Web Developer",
-                1000, 
+                1000,
                 "Front-End Developer",
-                1000,                
+                1000,
               ]}
               wrapper="span"
               speed={50}
-              style={{ fontSize: "1em", display: "inline-block", paddingLeft: "5px" }}
+              style={{
+                fontSize: "1em",
+                display: "inline-block",
+                paddingLeft: "5px",
+              }}
               repeat={Infinity}
             />
           </h2>
-          
-          <div className="flex justify-between pt-6 max-w-[200px] w-full text-gray-200 size-20 ">
-            <FaTwitter className="cursor-pointer hover:scale-110" size={30}/>
-            <FaLinkedinIn className="cursor-pointer hover:scale-110" size={30}/>
-            <FaGithub className="cursor-pointer hover:scale-110" size={30}/>            
-          </div>
 
+          <div className="flex justify-between pt-6 max-w-[200px] w-full text-gray-200 size-20 ">
+            <Link to="https://twitter.com/i_m_rohaannn">
+              <FaTwitter className="cursor-pointer hover:scale-110" size={30} />
+            </Link>
+
+            <Link to="https://github.com">
+              <FaGithub className="cursor-pointer hover:scale-110" size={30} />
+            </Link>
+            
+            <Link to="https://www.linkedin.com">
+              <FaLinkedinIn className="cursor-pointer hover:scale-110" size={30}/>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
