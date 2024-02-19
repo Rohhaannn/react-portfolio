7e5d0594a6d895c9ee51c51906de1f16868eeb08
Rohan <shingaderohan96@gmail.com>
2024-02-19 12:26:03 +0530

    about section updated

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import mypic from '../assets/mypic.jpg'
+import React from 'react';
+import mypic from '../../public/mypic.jpg'
 
 const About = () => {
+  
   return (
-    <div className='w-screen bg=[#ebeeee]'>
-      <div id="about" className="w-screen max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-        
-          <h1 className="text-4xl mb-10 font-bold text-center text-[#001b5e]"> About Me </h1>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi iusto minus expedita aspernatur quasi soluta minima eaque! Earum quod tenetur quasi ad! Est minus necessitatibus quibusdam voluptas ratione quam beatae. </p>
-        
+    <div className="w-screen bg-[#ebeeee]">
+      <div id="about" className="max-w-[1040px] mx-auto flex flex-col md:flex-row items-center p-4 py-16">
+        <div className="md:w-1/2 pr-8">
+          <img
+            className="w-58 h-56"
+            src={mypic}
+            alt="Rohan"
+          />
+        </div>
+        <div className="md:w-1/2">
+          <h1 className="text-4xl mb-4 font-bold text-[#001b5e]">About Me</h1>
+          <p className="text-lg">
+          I am a post-graduate, holding a Masters of Computer Applications (MCA) degree from Pune University. I also completed my Bachelor's degree in Computer Applications from Shivaji University, Kolhapur. With a strong academic foundation and expertise in computer science, I am dedicated to leveraging my knowledge to deliver exceptional results.
+          </p>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
