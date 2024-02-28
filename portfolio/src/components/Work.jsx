b1e918ea00084eef322f32e129a8b8a6e98d1ae2
Rohan <shingaderohan96@gmail.com>
2024-02-28 12:45:01 +0530

    work component updated

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -3,34 +3,39 @@ import WorkItem from './WorkItem';
 
 const data = [
   {
-    year: 2020,
-    role: "Software Engineer",
-    duration: "1 Year",
+    year: "Apr-2023 to June-2023",
+    company: "Solar Secure Solutions",
+    role: "FullStack Web Developer Intern",
+    location: "online",
+    duration: "2 Months",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, dolores quia. Doloribus harum architecto natus commodi alias fugit, optio voluptates molestiae ratione voluptas sit aut rerum ipsa enim error. Dolore?",
+      "My primary focus lies in developing basic projects using HTML, CSS, JavaScript, JQuery, Vanilla JS, and Bootstrap, integrating various animations to create interactive web pages and applications. Additionally, I have undertaken a significant project centered around the concept of 'Food Ordering,' employing technologies such as Node.js, MySQL, HTML, CSS, and JavaScript to deliver a comprehensive solution.",
   },
+
   {
-    year: 2017,
-    role: "Software Engineer",
+    year: "Mar-21 to Feb-22",
+    company: "Infosys",
+    role: "Technical Process Executive",
+    location: "Pune",
     duration: "1 Year",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, dolores quia. Doloribus harum architecto natus commodi alias fugit, optio voluptates molestiae ratione voluptas sit aut rerum ipsa enim error. Dolore?",
+      "I'm a skilled healthcare professional with expertise in data management, report generation, and process enhancement, bolstered by my proficiency in SQL. Collaborating effectively with cross-functional teams, I excel in identifying and implementing solutions. Additionally, my hands-on experience in HTML, CSS, and JavaScript, alongside tools like ServiceNow and JIRA, allows me to contribute to user-centric design and experience optimization. Committed to continuous improvement, I actively participate in seminars on Python and React.js.",
   },
+
   {
-    year: 2015,
-    role: "Software Engineer",
+    year: "Feb-19 to May-19",
+    company: "Web Soft IT Solutoins",
+    role: "Java / IOT Internship",
+    location: "Pune",
     duration: "1 Year",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, dolores quia. Doloribus harum architecto natus commodi alias fugit, optio voluptates molestiae ratione voluptas sit aut rerum ipsa enim error. Dolore?",
+    "Primarily focused on comprehending IoT programs utilizing Raspberry Pi, Arduino, and their interfacing with diverse modules and sensors. I have contributed to the development of projects aimed at assisting handicapped individuals, demonstrating a commitment to leveraging technology for inclusive solutions.",
   },
-  {
-    year: 2022,
-    role: "Software Engineer",
-    duration: "1 Year",
-    details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, dolores quia. Doloribus harum architecto natus commodi alias fugit, optio voluptates molestiae ratione voluptas sit aut rerum ipsa enim error. Dolore?",
-  },
+
 ];
+
+
+
 const Work = () => {
   return (
     <div className="w-screen bg-[#ebeeee]">
@@ -40,7 +45,9 @@ const Work = () => {
           <WorkItem
             key={idx}
             year={item.year}
+            company={item.company}
             role={item.role}
+            location={item.location}
             duration={item.duration}
             details={item.details}
           />
--- portfolio/src/components/WorkItem.jsx
+++ portfolio/src/components/WorkItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const WorkItem = ({year, role, duration, details}) => {
+const WorkItem = ({year, company, role, location,  duration, details}) => {
   return (
     <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
       <li className='mb-10 ml-4'>
         <div className='absolute w-3 h-3 bg-stone-400 rounded-full mt-1.5 -left-1.5 border-black'/>
         <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
           <span className='inline-block px-2 py-1 rounded-lg font-semibold text-white bg-[#001b5e]'>{year}</span>
+          <span className='text-lg font-bold text-[#01b5e]'>{company}</span>
           <span className='text-lg font-semibold text-[#01b5e]'>{role}</span>
+          <span className='text-lg font-semibold text-[#01b5e]'>{location}</span>
           <span className='my-1 text-sm font-normal leading-none text-stone-500'>{duration}</span>
         </p>
         <p className='my-2 text-base font-normal text-stone-600'>{details}</p>
