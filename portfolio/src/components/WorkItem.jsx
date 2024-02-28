2352d4d5a103322f120d5e78484ac2de94703596
Rohan <shingaderohan96@gmail.com>
2024-02-28 15:09:21 +0530

    work year styling updated

--- portfolio/src/components/WorkItem.jsx
+++ portfolio/src/components/WorkItem.jsx
@@ -6,7 +6,7 @@ const WorkItem = ({year, company, role, location,  duration, details}) => {
       <li className='mb-10 ml-4'>
         <div className='absolute w-3 h-3 bg-stone-400 rounded-full mt-1.5 -left-1.5 border-black'/>
         <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-          <span className='inline-block px-2 py-1 rounded-lg font-semibold text-white bg-[#001b5e]'>{year}</span>
+          <span className='inline-block px-2 py-2 rounded-lg font-semibold text-white bg-[#001b5e]'>{year}</span>
           <span className='text-lg font-bold text-[#01b5e]'>{company}</span>
           <span className='text-lg font-semibold text-[#01b5e]'>{role}</span>
           <span className='text-lg font-semibold text-[#01b5e]'>{location}</span>
