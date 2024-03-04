98a02ad2e36cde86020104ab849cacaa13261d33
Rohan <shingaderohan96@gmail.com>
2024-03-04 17:25:34 +0530

    navbar for small screen updated

--- portfolio/src/components/Sidenav.jsx
+++ portfolio/src/components/Sidenav.jsx
@@ -13,10 +13,10 @@ const Sidenav = () => {
 
   return (
     <div>
-      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden text-white size-5'/>
       {
         nav ? (
-          <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+          <div className='fixed w-full h-screen  flex flex-col justify-center items-center z-20'>
 
             {/* Home */}
             <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
